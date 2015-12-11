fe490697c79937becb54cc48bdc0282196927569
Oleksandr Yatskanich <yatskanich.sasha@gmail.com>
2015-12-11 14:12:42 +0200

    fix

--- after-install.py
+++ after-install.py
@@ -39,23 +39,24 @@ def change_apache_user(user_name):
 
     os.system('sudo mv /tmp/env.tmp /etc/apache2/envvars')
 
+
+def make_www_in_home(user_name):
+    os.system('sudo cp /etc/apache2/apache2.conf /tmp/apache_conf.tmp')
+
+    with open('/tmp/apache_conf.tmp', 'a') as new_apache_conf:
+        conf = '<Directory /home/{username}/www/>\n'.format(username=user_name)
+        conf += '\tOptions Indexes FollowSymLinks\n'
+        conf += '\tAllowOverride All\n'
+        conf += '\tRequire all granted\n'
+        conf += '</Directory>'
+        new_apache_conf.write(conf)
+
+    os.system('sudo mv /tmp/apache_conf.tmp /etc/apache2/apache2.conf')
+
+
 if __name__ == "__main__":
-    install('commands.txt')
-
-    with open('/etc/apache2/apache2.conf', 'a') as apache_conf:
-        apache_conf.write("ServerName localhost")
-
-    ss = input('Generate SSH key (y/n):')
-    if ss == 'y':
-        ssh_generator()
-
-    ch = input('Install other soft (y/n):')
-    if ch == 'y':
-        install_soft()
-
     user_name = getpass.getuser()
-    mess = 'Run apache with current user {0} (y/n):'.format(user_name)
-
-    uc = input(mess)
-    if uc == 'y':
-        change_apache_user(user_name)
+    mess = 'Make www dir in {username} home folder? (y/n):'.format(username=user_name)
+    user_choice = input(mess)
+    if user_choice == 'y':
+        make_www_in_home(user_name)
